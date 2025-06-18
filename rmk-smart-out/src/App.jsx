962991775865d88147f573585ed5c7c2bfbbb102
HarishE34 <harish260905@gmail.com>
2025-06-18 16:34:08 +0530

    commit by harish

--- rmk-smart-out/src/App.jsx
+++ rmk-smart-out/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-// import LandingPage from './components/LandingPage/LandingPage'
+import LandingPage from './components/LandingPage/LandingPage'
+import Sidebar from './components/CounsellorLogin/Attendace-system/Sidebar'
 import { Attendance } from './components/CounsellorLogin/Attendace-system/Attendance'
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div>
       {/* <LandingPage /> */}
       <Attendance/>
+      <Sidebar/>
     </div>
   )
 }
--- rmk-smart-out/src/components/CounsellorLogin/Attendace-system/Attendance.jsx
+++ rmk-smart-out/src/components/CounsellorLogin/Attendace-system/Attendance.jsx
@@ -28,45 +28,48 @@ export const Attendance = () => {
   ];
 
   return (
-    <div style={{ display: "flex", 
-      backgroundColor: '#eee', 
-     }}>
-      <div style={{ width: "15%", height: "100vh", border: "1px solid black",backgroundColor:'white' }}>
-          <div className="container">
-            <div className="logo-container">
-              <img src={rmklogo} alt="RMK Logo" className="logo" />
-              <h2 className='side '>INFORMATION TECHNOLOGY</h2>
-              <h3 className="title">STUDENT ATTENDANCE SYSTEM</h3>
-              <div className="table-scroll-container">
-                <table className="blacky">
-                  <thead className='head'>
-                    <tr>
-                      <th>S.I</th>
-                      <th>NAME</th>
-                      <th>REG.NO</th>
-                      <th>YEAR-SEC</th>
-                      <th>ATTENDANCE ENTRY</th>
+    <div style={{
+      display: "flex",
+      backgroundColor: '#eee',
+    }}>
+      <div style={{ width: "15%", height: "100vh", border: "1px solid black", backgroundColor: 'white' }}>
+        <div className="container">
+          <div className="logo-container">
+            <img src={rmklogo} alt="RMK Logo" className="logo" />
+             <div className="title-area">
+            <h2 className='side '>INFORMATION TECHNOLOGY</h2>
+            <h3 className="title">STUDENT ATTENDANCE SYSTEM</h3>
+            <div className="table-scroll-container">
+              <table className="blacky">
+                <thead className='head'>
+                  <tr>
+                    <th>S.I</th>
+                    <th>NAME</th>
+                    <th>REG.NO</th>
+                    <th>YEAR-SEC</th>
+                    <th>ATTENDANCE ENTRY</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {students.map((student, index) => (
+                    <tr key={student.id}>
+                      <td>{index + 1}</td>
+                      <td>{student.name}</td>
+                      <td>{student.reg}</td>
+                      <td>{student.year}</td>
+                      <td className="gap">
+                        <button className='gun'>PRESENT</button>
+                        <button className='bun'>ABSENT</button>
+                      </td>
                     </tr>
-                  </thead>
-                  <tbody>
-                    {students.map((student, index) => (
-                      <tr key={student.id}>
-                        <td>{index + 1}</td>
-                        <td>{student.name}</td>
-                        <td>{student.reg}</td>
-                        <td>{student.year}</td>
-                        <td className="gap">
-                          <button className='gun'>PRESENT</button>
-                          <button className='bun'>ABSENT</button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-        </div>
+      </div>
+      </div>
+    </div>
   );
 };
