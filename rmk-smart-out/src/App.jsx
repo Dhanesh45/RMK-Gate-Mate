232747a0707a8ec5dcdd982cbc321d633a054758
JAGADISH06KG <Kgjagadish06@gmail.com>
2025-06-22 13:03:29 +0530

    comitted by j

--- rmk-smart-out/src/App.jsx
+++ rmk-smart-out/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import LandingPage from './components/LandingPage/LandingPage'
+import ApprovalPage from './components/ApprovalPage/ApprovalPage'
 
 const App = () => {
   return (
     <div>
       <LandingPage />
+      <ApprovalPage />
     </div>
   )
 }
---
+++ rmk-smart-out/src/components/ApprovalPage/ApprovalPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ApprovalPage = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default ApprovalPage
