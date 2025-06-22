4a56e7617902bb6b70ca83d2ab4d8d4c2cb1e68e
dhanraj gunasekar <haridhanarj29@gmail.com>
2025-06-22 16:33:34 +0530

    changes done

---
+++ rmk-smart-out/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Dashboardhead from './components/YearCoordinatorLogin/dashboard/Dashboardhead';
+import './components/YearCoordinatorLogin/dashboard/Dashboard.css';
+import'./index.css'
+
+
+
+
+
+function App() {
+  return (
+    <div >
+      
+      <Dashboardhead  />
+      
+    </div>
+  )
+}
+
+export default App
