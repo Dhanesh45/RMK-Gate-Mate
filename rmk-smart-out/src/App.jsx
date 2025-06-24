5560ae2a2187efb43bd63645b1d9372763d2a11e
dhanesh <dhaneshprabhupro@gmail.com>
2025-06-24 10:36:04 +0530

    Changes

--- rmk-smart-out/src/App.jsx
+++ rmk-smart-out/src/App.jsx
@@ -3,14 +3,16 @@ import LandingPage from './components/LandingPage/LandingPage'
 import CommonLogin from './components/LandingPage/CommonLogin'
 import StudentApproval from './components/CounsellorLogin/StudentApproval'
 import Sidebar from './components/CounsellorLogin/Sidebar'
+import StudentLogin from './components/StudentLogin/StudentLogin'
 
 const App = () => {
   return (
     <div>
       {/* <LandingPage /> */}
       {/* <CommonLogin /> */}
-      <StudentApproval />
+      {/* <StudentApproval /> */}
       {/* <Sidebar /> */}
+      <StudentLogin />
       
     </div>
   )
---
+++ rmk-smart-out/src/components/StudentLogin/StudentLogin.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const StudentLogin = () => {
+  return (
+    <div>
+        <div>
+            {/* image part */}
+            <div>
+
+            </div>
+
+            {/* Form part */}
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default StudentLogin
