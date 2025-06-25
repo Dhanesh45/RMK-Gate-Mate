4e9871f145683dd93d91df563ef317521da8a2a8
Dhinesh31125 <dhinesh31125@gmail.com>
2025-06-25 21:23:48 +0530

    Dhinesh

--- rmk-smart-out/src/App.jsx
+++ rmk-smart-out/src/App.jsx
@@ -1,14 +1,10 @@
-import React from 'react'
-// import LandingPage from './components/LandingPage/LandingPage'
-import'./components/StudentLogin/student-details/StudentDetails.css'
-import StudentDetails from './components/StudentLogin/student-details/StudentDetails'
+
 
 
 const App = () => {
   return (
     <div>
-      {/* <LandingPage /> */}
-      <StudentDetails />
+     
     </div>
   )
 }
