636443f38086565cd28c0bb9eb65430c887ecdca
dhanesh <dhaneshprabhupro@gmail.com>
2025-06-25 21:28:06 +0530

    Dhan

--- rmk-smart-out/src/App.jsx
+++ rmk-smart-out/src/App.jsx
@@ -1,20 +1,9 @@
-import React from 'react'
-import LandingPage from './components/LandingPage/LandingPage'
-import CommonLogin from './components/LandingPage/CommonLogin'
-import StudentApproval from './components/CounsellorLogin/StudentApproval'
-import Sidebar from './components/CounsellorLogin/Sidebar'
-import StudentLogin from './components/StudentLogin/StudentLogin'
-import CounsDashboard from './components/CounsellorLogin/Counsellors-Dashboard/CounsDashboard'
+
 
 const App = () => {
   return (
     <div>
-      {/* <LandingPage /> */}
-      {/* <CommonLogin /> */}
-      {/* <StudentApproval /> */}
-      {/* <Sidebar /> */}
-      <StudentLogin />
-      <CounsDashboard />
+     
       
     </div>
   )
