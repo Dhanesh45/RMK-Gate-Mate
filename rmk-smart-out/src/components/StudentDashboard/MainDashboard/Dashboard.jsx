41dcd17436ba74d89e88fd7937473dcd74708e74
HarishE34 <harish260905@gmail.com>
2025-06-29 18:59:58 +0530

    commit by harish

--- rmk-smart-out/src/components/StudentDashboard/MainDashboard/Dashboard.jsx
+++ rmk-smart-out/src/components/StudentDashboard/MainDashboard/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Dashboard.css';
+import dashphoto from '../../../assets/Dashboardimg/dhashimg1.jpg';
+import profilephoto from '../../../assets/Dashboardimg/dhashimg2.jpg';
 import STUsideBar from './STUsideBar';
 import rmklogo from "../../../assets/LandingPageimg/rmklogo.png";
 const Dashboard = () => {
@@ -11,49 +13,46 @@ const Dashboard = () => {
                     <img src={rmklogo} className='logoo' />
                     <h1 className="side">INFORMATION TECHNOLOGY</h1>
                 </div>
-                <div className="profile-card">
-                    <img
-                        className="cover-img"
-                        src="https://img.freepik.com/free-vector/mountain-landscape-with-pine-trees_1308-120845.jpg"
-                        alt="cover"
-                    />
-                    <img
-                        className="profile-pic"
-                        src="https://randomuser.me/api/portraits/women/44.jpg"
-                        alt="student"
-                    />
-                    <h3>Student Name</h3>
-                    <h9>alexarawles@gmail.com</h9>
-                    <p>
-                        Register Number :111723203000 | Degree :B.Tech - Information Technology | Batch
-                        :2027 | College :R.M.K. Group of Institutions
-                    </p>
+                  <div style={{justifyItems:"center",width:"100%",height:"50%"}}>
+                <div style={{width:"94%",height:"100%",backgroundColor:"white",borderRadius:"20px",position:"relative"}}>
+                    <div style={{width:"100%",height:"45%"}}>
+                        <img src={dashphoto} alt=""  style={{width:"100%",height:"100%",borderRadius:"20px 20px 20px 20px"}}/>
+                    </div>
+                    <div style={{position:"absolute",width:"10%",height:"35%",top:"20%",left:"10%"}}>
+                        <img src={profilephoto} alt="" style={{width:"100%",height:"100%",borderRadius:"50%",border:"10px solid white"}}/>
+                    </div>
+                    <div style={{width:"100%",height:"50%",padding:"5% 0% 0% 8%"}}>
+                        <h1 style={{paddingBottom:"0.75%",fontSize:"25px",fontWeight:"550"}}>YEAR CO ORDINATOR NAME</h1>
+                        <p  style={{paddingBottom:"0.75%",fontSize:"15px",color:"gray"}}>YR CO@gmail.com</p>
+                        <b><p  style={{paddingBottom:"0.75%",fontSize:"15px"}}>Number of Students: 125   |   Degree : B.Tech - Information Technology  |   College :R.M.K. Group of Institutions</p>
+                   </b> </div>
                 </div>
+            </div>
 
-                    <div className="frame-row">
-                        <div className="frame-box">
-                            <p>PRINCIPAL</p>
-                            <img src="https://randomuser.me/api/portraits/men/45.jpg" alt="Principal" />
-                            <strong>Dr. K.A. Mohamed Junaid</strong>
-                        </div>
-                        <div className="frame-box">
-                            <p>HEAD OF DEPARTMENT</p>
-                            <img src="https://randomuser.me/api/portraits/women/46.jpg" alt="HOD" />
-                            <strong>Dr. M. Sheerin Banu</strong>
-                        </div>
-                        <div className="frame-box">
-                            <p>YEAR COORDINATOR</p>
-                            <img src="https://randomuser.me/api/portraits/women/47.jpg" alt="Year Coordinator" />
-                            <strong>Dr. A. Annalakshmi</strong>
-                        </div>
-                        <div className="frame-box">
-                            <p>COUNSELLOR</p>
-                            <img src="https://randomuser.me/api/portraits/women/48.jpg" alt="Counsellor" />
-                            <strong>Dr. M. Sheerin Banu</strong>
-                        </div>
+                <div className="frame-row">
+                    <div className="frame-box">
+                        <p>PRINCIPAL</p>
+                        <img src="https://randomuser.me/api/portraits/men/45.jpg" alt="Principal" />
+                        <strong>Dr. K.A. Mohamed Junaid</strong>
+                    </div>
+                    <div className="frame-box">
+                        <p>HEAD OF DEPARTMENT</p>
+                        <img src="https://randomuser.me/api/portraits/women/46.jpg" alt="HOD" />
+                        <strong>Dr. M. Sheerin Banu</strong>
+                    </div>
+                    <div className="frame-box">
+                        <p>YEAR COORDINATOR</p>
+                        <img src="https://randomuser.me/api/portraits/women/47.jpg" alt="Year Coordinator" />
+                        <strong>Dr. A. Annalakshmi</strong>
+                    </div>
+                    <div className="frame-box">
+                        <p>COUNSELLOR</p>
+                        <img src="https://randomuser.me/api/portraits/women/48.jpg" alt="Counsellor" />
+                        <strong>Dr. M. Sheerin Banu</strong>
                     </div>
                 </div>
             </div>
+        </div>
     );
 };
 
